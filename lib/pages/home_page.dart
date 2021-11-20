d74668722183f2c3956013a80e3d832829a5c35d
bhawnasaroha <30bhawna95@gmail.com>
2021-11-21 04:54:11 +0530

    grid view used for products

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../constants.dart';
 import '../widgets/item_widget.dart';
 import '../models/catalog.dart';
 import '../widgets/drawer.dart';
@@ -44,11 +45,49 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items?.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items![index],
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
                 ),
+                itemCount: CatalogModel.items!.length,
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items![index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: GridTile(
+                      header: Container(
+                        decoration: BoxDecoration(
+                          color: primaryColor,
+                        ),
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          item.name,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          item.price.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               )
             : Center(
                 child: CircularProgressIndicator(),
